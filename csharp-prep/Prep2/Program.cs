ea229f6d17057d961481eabdd279d69ab08cb714
koheimaeda <ohanaryosakiko@gmail.com>
2024-03-02 11:17:26 +0900

    update

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int persentage = int.Parse(answer);
+
+        string letter = "";
+
+        if (persentage >= 90)
+        {
+            letter = "A";
+        }        
+        else if (persentage >= 80)
+        {
+            letter = "B";
+        }       
+        else if (persentage >= 70)
+        {
+            letter = "C";
+        }       
+        else if (persentage >= 60)
+        {
+            letter = "D";
+        }       
+        else
+        {
+            letter = "F";
+        }       
+
+        Console.WriteLine($"Your letter grade is {letter}.");
+
+        if (persentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass the class. Good luck next time!");
+        }
+
     }
 }
